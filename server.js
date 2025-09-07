d1b6f3dbf033ee988bbe3c865fb64372c1ba2f69
maisajalsharma <2k24.cs1n.2413793@gmail.com>
2025-09-07 18:08:06 +0530

    remove wrong

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fetch = require('node-fetch');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
